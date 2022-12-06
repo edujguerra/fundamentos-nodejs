4f2c7cd570dfc0927dc0c81135bf19f0813d1790
Eduardo J. Guerra <edujguerra@gmail.com>
2022-12-05 22:02:24 -0300

    2212052202

--- src/index.js
+++ src/index.js
@@ -19,12 +19,17 @@ app.get("/account",(request,response) => {
 
 app.post("/account",(request,response) => {
     const { cpf, name } = request.body;
-    const id = uuidv4();
 
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({error:"Customer already exists"});
+    }
+    
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     })
 
